b47d0011ef605de74973b362426a928837107e80
DimarsMars <dimasrangga620@gmail.com>
2025-03-21 01:39:39 +0800

    update main.go

--- main.go
+++ main.go
@@ -28,10 +28,51 @@ func main() {
 
 	router := gin.Default()
 
-	router.GET("/users", func(c *gin.Context) {
-		var users []User
-		db.Find(&users)
+	router.GET("/user", func(c *gin.Context) {
+		var user []User
+		db.Find(&user)
+		c.JSON(http.StatusOK, gin.H{"data": user})
+	})
+
+	router.POST("/user", func(c *gin.Context) {
+		var user User
+		c.BindJSON(&user)
+		db.Create(&user)
+		c.JSON(http.StatusOK, gin.H{"data": user})
+	})
+
+	router.GET("/users/:id", func(c *gin.Context) {
+		var users User
+		id := c.Param("id")
+
+		db.First(&users, id)
 		c.JSON(http.StatusOK, gin.H{"data": users})
+	})
+
+	router.PUT("/users/:id", func(c *gin.Context) {
+		var users User
+		id := c.Param("id")
+
+		if err := db.First(&users, id).Error; err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "User Not Found"})
+			return
+		}
+
+		if err := c.ShouldBindJSON(&users); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Format data tidak valid"})
+			return
+		}
+
+		db.Save(&users)
+		c.JSON(http.StatusOK, gin.H{"data": users})
+	})
+
+	router.DELETE("/users/:id", func(c *gin.Context) {
+		var users User
+		id := c.Param("id")
+
+		db.Delete(&users, id)
+		c.JSON(http.StatusOK, gin.H{"message": "User berhasil dihapus"})
 	})
 
 	router.Run(":3000")
